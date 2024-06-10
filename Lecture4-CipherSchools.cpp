fc03f4e3f5e8df45762f213bc1cf6a2167789a65
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-06-10 21:13:55 +0530

    Create Lecture4-CipherSchools.cpp

---
+++ Lecture4-CipherSchools.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+  //Declaring 2 variables
+  int a;
+  int b;
+  int c;
+  //Assigning values to them
+  a=3;
+  b=5;
+  //Adding them both
+  c=a+b;
+  cout<<c;
+  
+}
