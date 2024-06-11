d7a1126779180ddcc812f69013d9cc6219f2eaf9
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-06-12 03:38:16 +0530

    Create LectureFourteen-CipherSchools.cpp

---
+++ LectureFourteen-CipherSchools.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void printstuff(){
+  cout<<"Writing my first Function";
+}
+int main(){
+  printstuff();
+  return 0;
+}
+
+____________________________________________________
+//Function to add two numbers
+#include <iostream>
+using namespace std;
+int sum(int a, int b){
+  int c;
+  c=a+B;
+  return c;
+}
+int main(){
+  int a,b;
+  a=3;
+  b=5;
+  sum(a,b)
+  return 0;
+}
