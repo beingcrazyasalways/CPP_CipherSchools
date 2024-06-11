5adcc82cd1b2e34de2649eb144858482a24e89b1
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-06-11 22:53:28 +0530

    Create Lecture8-CipherSchools.cpp

---
+++ Lecture8-CipherSchools.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a[4];
+  cout<<"Give 5 integer as input"<<endl;
+  for(i=0;i<5;i++){
+              cin>>a[i]<<" ";
+  }
+  return 0;
+}
