fb5abe36394aebd6fd1266ab9e987bf559b1dfe0
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-06-11 23:16:09 +0530

    Create LectureEleven-CipherSchools.cpp

---
+++ LectureEleven-CipherSchools.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int maain(){
+  int=i;
+  i=1;
+  while(i<101){
+          cout<<i<<" ";
+          i++;
+    }
+  return 0;
+}
+
+_______________________________
+
+#include <iostream>
+using namespace std;
+int main(){
+    int i;
+    int input;
+    i=100;
+
+    for(i=i;i<101;i++){
+          cin>>input;
+          if(input == 65){
+                    cout<<"Congrats you have guessed the right answer"<<endl;
+                    break;
+          }
+    }
+  return 0;
+}
