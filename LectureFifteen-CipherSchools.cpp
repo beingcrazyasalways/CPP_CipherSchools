25a4541c66fdb1f4dd2aecaf3fe1b2cdc7b5ac9c
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-06-12 03:41:45 +0530

    Create LectureFifteen-CipherSchools.cpp

---
+++ LectureFifteen-CipherSchools.cpp
@@ -0,0 +1,16 @@
+int compare (int a, int b){
+  if (a>b){
+        return 0;
+  }
+  else{
+      return b;
+  }
+}
+_____________________________________
+
+int compare(int a, int b){
+  if (a==5){
+        return 1;
+  }
+  return 0;
+}
