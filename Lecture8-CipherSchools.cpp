7e6e520da43926522872bee68c99e82503bb41cd
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-06-11 22:58:24 +0530

    Update Lecture8-CipherSchools.cpp

--- Lecture8-CipherSchools.cpp
+++ Lecture8-CipherSchools.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 int main(){
   int a[4];
+  int i;
   cout<<"Give 5 integer as input"<<endl;
   for(i=0;i<5;i++){
+              cin>>a[i]<<" ";
+  }
+  return 0;
+}
+
+_______________________________________________
+
+//Print 10 number consecutively [ARRAY] in reverse order
+#include <iostream>
+using namespace std;
+int main(){
+  int a[9];
+  int i;
+  cout<<"Give 10 integer as input"<<endl;
+  for (i=0;i<10;i++){
+    cin>>a[i];
+  }
+  cout<<"The 10 given input are: "<<endl;
+  for(i=9;i>=0;i--){
               cin>>a[i]<<" ";
   }
   return 0;
