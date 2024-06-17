c5bd17724c1b4d61074ae986d97dad7a507c31dc
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-06-17 13:43:54 +0530

    Create Lecture20-CipherSchools.cpp

---
+++ Lecture20-CipherSchools.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+  long long int a;
+  int b;
+  cout<<"The size of int is: "<<sizeof(b)<<"The size of long long int: "<<sizeof(a)<<endl;
+  cout<<"The address of a is: "<<&a<<"The address of b is : "<<&b<<endl;
+  return 0;
+}
