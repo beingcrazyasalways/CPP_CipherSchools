5e0d4e3907bf3f9fa960609f79b6cf85c921bfe6
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-06-18 15:54:46 +0530

    Update Lecture22-CipherSchools.cpp

--- Lecture22-CipherSchools.cpp
+++ Lecture22-CipherSchools.cpp
@@ -9,3 +9,18 @@ int main(){
   count<"The value of s form the pointer using"<<*p<<endl;
   return 0;
 }
+___________________________________________________________________
+
+#include <iostream>
+using namespace std;
+int main(){
+  int a;
+  int b;
+  int c;
+  p=&a;
+  q=&a;
+
+  a=2;
+  b=c;
+  c=*p+*q;
+  cout<<"The Value of c is: "<<c;
