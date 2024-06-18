318d8b57a56a9880e14073c9caabaf452dcbcac5
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-06-18 16:00:24 +0530

    Create Lecture23-CipherSchools.cpp

---
+++ Lecture23-CipherSchools.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a=5;
+  int *x;
+  int =&a;
+  cout<<"The address of a : "<<x<<"The value of x+1 is"<<x+1;
+  return 0;
+}
+
+__________________________________________________________________
+
+int a [5];
+int *x;
+x=&a[0];
+int i;
+for (i=0; i<b;i++){
+    cout<<a[i]<<endl;
+ }
+ return 0;
+}
