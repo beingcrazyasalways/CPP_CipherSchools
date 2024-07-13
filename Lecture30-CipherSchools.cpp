4c7f4e05e2c0474fb739e48c8f94380defdf36a5
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-07-13 21:47:42 +0530

    Lecture30-CipherSchools.cpp

--- Lecture30-CipherSchools.cpp
+++ Lecture30-CipherSchools.cpp
@@ -1,16 +1,60 @@
-#include <iostream>
+include<iostream>
 using namespace std;
-class Complex{
-  private:
-  float real;
-  float img;
-  public;
-  void set_parameters(int real,int img){
 
+class Student{
+	public:
+		string name;
+		int attendance;
+		float total_marks;
+		
+		void calculate_percentage(){
+			cout<<total_marks<<"%";
+		}
+};
 
-  void SqdistancefromOrigin(){
-    float dist;
-    dist = real*real +img*img;
-    cout<<"the distance from origin is "<<dist;
-    return;
-  }
+class bank_client{
+	private:
+		int credit_no;
+		int cvv;
+		string password;
+	
+	public:
+		string name;
+		string user_name;
+		
+		void set_credit_card_no(int number){
+//			credit_no = number;
+			this->credit_no = number;
+			cout<<"the address of instance is "<<this<<endl;
+		}
+		
+		void get_credit_card_no(){
+			cout<<"credit card number is "<<credit_no<<endl;
+		}	
+};
+
+int main(){
+//	Student a,b,c;
+//	
+//	a.name = "rohit";
+//	a.attendance = 90;
+//	a.total_marks = 69;
+//	
+//	a.calculate_percentage();
+
+	bank_client a,b,c;
+//	a.name = "rohit";
+//	a.password = "enjoyc++";
+//	a.cvv=788;
+//	a.credit_no = 12221991;
+	
+//	cout<<"The password of rohit is: "<<a.password;
+	
+	a.set_credit_card_no(1221991);
+	a.get_credit_card_no();
+	
+	cout<<"the address of instance is "<<&a;
+	
+	return 0;
+}
+
