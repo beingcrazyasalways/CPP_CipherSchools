565eb546381dcd01a71417198fbb6106cd6d0c7b
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-07-13 10:07:26 +0530

    Create Lecture25-CipherSchools.cpp

---
+++ Lecture25-CipherSchools.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void min and max(int* a,int*min){
+  *min=1;
+  *max=5;}
+  int main (){
+    int a[5];
+    int i;
+    for (i=o;i<b;i++){
+          if (a[i]>large){
+              large=a[i];
+          }
+          if(a[i]<small){
+          small= a[i];
+          }
+    }
+
+-----------------------------------------
+int main()[
+  int a[5];
+   int min,max;
+   int i;
+   for (i=0;i<6;i++){
+        a[i] = i;
