b7f803490db0d25bcf821e65ffc58ebe7529c74e
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-07-13 21:02:38 +0530

    Create Lecture27-CipherSchools.cpp

---
+++ Lecture27-CipherSchools.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string.
+using namespace std;
+int main(){
+  sttring a;
+  string b;
+  cin>>a>>b;
+  cout<<"The strng which you have given as input are"<<a<<b;
+  string c;
+  c=a+b;
+  cout<<"The combined word is:"<<c;
+  cout<<"The lenght of combined name is<<c.length();
+reutn 0;
+}
