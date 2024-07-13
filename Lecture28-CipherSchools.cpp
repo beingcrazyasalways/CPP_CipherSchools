6ed98c488053007a31deaac84eb944b4afd6e9fe
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-07-13 21:10:19 +0530

    Create Lecture28-CipherSchools.cpp

---
+++ Lecture28-CipherSchools.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class Student{
+  public;
+  String name;
+  int attendance;
+  float total_marks;
+  void calculate_Percentage(){
+    cout<<total_marks<<"%";
+  }
+
+}
+int main(){
+  Student a,b,c
+  a.name="Rohit";
+  a.attendance =99;
+  a.total_marks=99;
+  a.calculate_percentage():
+  return 0;
