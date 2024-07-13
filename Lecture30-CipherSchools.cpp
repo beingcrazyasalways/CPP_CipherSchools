0295d95b6a791ba16fa8cdbee369758196a0fbd2
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-07-13 21:45:17 +0530

    Create Lecture30-CipherSchools.cpp

---
+++ Lecture30-CipherSchools.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class Complex{
+  private:
+  float real;
+  float img;
+  public;
+  void set_parameters(int real,int img){
+
+
+  void SqdistancefromOrigin(){
+    float dist;
+    dist = real*real +img*img;
+    cout<<"the distance from origin is "<<dist;
+    return;
+  }
