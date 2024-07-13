a9b23a074344f8f515fea35b7c105beac7f655aa
Suryansh <118935802+beingcrazyasalways@users.noreply.github.com>
2024-07-13 11:38:58 +0530

    Lecture26-CipherSchools.cpp

--- Lecture26-CipherSchools.cpp
+++ Lecture26-CipherSchools.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+using namespace std;
+int main(){
+  char name[1000];
+  cout<<"Enter your name:";
+  int i=0;
+  while(i){
+      cin>>name[i];
+      if cname[i]=='#')break;
+      i++;
+  }
+  i=0;
+cout<<"The name you have just entered is"<<endl;
+while(name[i]!='#'){
+      cout<<name[i];
+      i++;
+}
+String name;
+cin>>name;
+cout<<;
